eb0348765c73f1b9c1b6404528cb97c9dbc858ee
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-04-04 22:06:18 +0400

    Add Python

---
+++ Python/PyApp/PyApp.py
@@ -0,0 +1,85 @@
+# ctrl + K + C
+# ctrl + K + U
+
+# string
+# print("Hello World!")
+# print(type("Hello World!"))
+
+# integer
+# print(123)
+# print(type(123))
+
+# float
+# print(12.7)
+# print(type(12.7))
+
+
+
+# variables
+# camelCase
+
+# myPhoneNumber="055 784 78 78"
+# print(myPhoneNumber)
+# print(type(myPhoneNumber))
+
+# print("Input message: ")
+# consoleInput = input()
+
+# print("Message: '" + consoleInput + "'")
+
+
+# + - / * %
+
+# +
+# print(2 + 3)
+# number = 2 + 3 + 4 + 1
+# print(number + 100)
+# print(10 + 12.5)
+
+# -
+# number = 10 - 34
+# print(number + 100)
+
+# / or //
+# number = 10 // 3
+# print(number)
+
+# *
+# x = 123.3
+# y = 34
+# print(x * y)
+
+# first = "one"
+# second = "two"
+# print("test", first, second)
+# message = f"{first} {second}"
+# print(message)
+
+
+## Hello App
+##print("Input your name: ")
+#name = input("Input your name: ")
+
+##print("Input your surname: ")
+#surname = input("Input your surname: ")
+
+#print(f"Hello, I am {name} {surname}")
+
+#numberStr = input("Input number: ")     # "123" # "test"
+#number = int(numberStr)                 # 123   # error
+#print(type(number))                     # int   # -
+
+
+
+#number = float("3.24")
+#myStringNumber = str(1.23)
+#print(type(myStringNumber))
+
+
+#str = "12"
+
+#number = int(str) + 10;
+
+#print(type(number), number)
+
+#print(type(f"{int('123')}"))
