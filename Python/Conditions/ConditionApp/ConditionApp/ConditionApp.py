cd9a16ef15696e5e9ea0817c5163ab4a8ed945e2
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-04-08 21:58:38 +0400

    Add operators

---
+++ Python/Conditions/ConditionApp/ConditionApp/ConditionApp.py
@@ -0,0 +1,66 @@
+# string integer float
+
+#num = "Test"
+#num = 4325345345
+#num = 123.
+#print(type(num))
+
+# Bad practice
+#True = 123
+#'qweqwe = 124;
+#11231asda = 547
+#число = 123
+#if = "qwerty"
+
+# boolean
+#visa = True
+
+# + - * ** / // %
+
+#y = 2
+#x = 4
+
+#print(int(y / x))
+#print(y // x)
+
+#print(y % x)
+
+#print(y ** x)
+
+#print("Hello World!")
+
+#num = -10
+
+#num = num * 5           # -50
+#num = num - 100         # -150
+#num = int(num / 2)      # -75
+#num = num + 12345       # 12270
+
+#print(num)
+
+
+num = 123
+
+# num += 10
+# num = num + 10
+
+#num = num % 10
+#num %= 10
+
+#print(num)
+
+
+
+#wallet = 1000.
+#salary = float(input("Input your salary: "))
+#wallet += salary
+#print(wallet)
+
+
+
+#number = int(input("Input number: "))
+#power = int(input(f"Input power to {number}: "))
+
+#result = number ** power
+
+#print(result)
