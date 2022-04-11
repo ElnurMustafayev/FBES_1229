c7393d565c9debe1545122533c5c30d029945e68
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-04-11 21:54:40 +0400

    Add Conditions

---
+++ Python/Conditions/ConditionsApp/ConditionsApp/ConditionsApp.py
@@ -0,0 +1,95 @@
+# print(3 * 2 + 3) 9
+# print(3 * (2 + 3) / 3)
+
+# num = (10 // 3) ** 2;
+# print(num)
+
+# + - / // * ** %
+
+# > >= < <= != ==
+
+# print(8 != -8) True
+# print(8 >= 2) True
+# print(8 <= -2) False
+
+# print(2 > (8 + 10))
+
+#x = 10
+#y = 23
+#z = -7
+
+#print((x + y + z - 100) > 0)
+
+#condition1 = 10 > (5 + 1)
+#condition2 = 0 == 0
+
+#print((condition1 - condition2) != 8)
+
+
+# and or not
+
+#visa = True
+#money = 42000.
+
+#willGo = money >= 41000. and visa == True
+
+#print(f"I will go to USA: {willGo}")
+
+# print(10 < 5 and 7 > 12)
+
+#print(10.5 >= 10.6)
+#print("Elnur" != "Umid")
+
+#firstWord = input()
+#secondWord = input()
+
+#print(firstWord == secondWord)
+
+
+#print(f"Hello, {name}")
+
+#visa = True
+#money = float(input())
+
+#requirements = money >= 41000. and visa == True
+#print(requirements)
+
+#number = 0
+#print(bool(number))
+
+#visa = False
+#money = 42_000.
+
+#print(money >= 41_000 and visa and bool(10))
+
+
+#knowHowToDrive = False
+#money = 800.
+
+#print(knowHowToDrive or money >= 1500.)
+#print(bool((10 - 8) / 2) or False)
+#print(False and True or False)
+
+    
+#age = 17
+
+#if age >= 18:
+#    print("User is Adult")
+
+#if age < 18:
+#    print("Error: User is not Adult!")
+
+
+# 9 -> str * 9
+# 11 -> "ERROR Message"
+
+#word = input("Enter word: ")
+#count = int(input("Enter count for repeat: "))
+
+#if count > 10:
+#    print("Error")
+#if count <= 10:
+#    result = word * count
+#    print(result)
+
+#print(not(10 > 5))
