8c2dfff516b2217389ab47f9d31d8c5b7dd2fe1c
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-04-22 21:57:26 +0400

    Add While

---
+++ Python/Home work/app.py
@@ -0,0 +1,3 @@
+name = input("Input your name: ")
+
+print(f"Hello, {name}")
---
+++ Python/Loops/WhileApplication/WhileApplication/WhileApplication.py
@@ -0,0 +1,60 @@
+#while bool(0):
+#    print("Iteration start")
+#    print("...")
+#    print("Iteration end")
+
+
+#flag = True
+
+#while flag:
+#    print("Iteration start")
+#    print("...")
+#    flag = False
+#    print("Iteration end")
+
+
+
+
+
+
+# FLAG
+#flag = True
+#counter = 0
+
+#while flag:
+#    if counter == 5:
+#        flag = False
+    
+#    counter += 1
+#    print(counter)
+
+
+# BREAK
+#counter = 0
+
+#print("start")
+
+#while True:
+#    counter += 1
+#    print(counter)
+
+#    if counter == 5:
+#        break
+
+#print("end")
+
+
+#counter = 0
+
+#print("start")
+
+#while counter != 4:
+#    counter += 1
+#    print(counter)
+
+#print("end")
+
+
+#import random
+
+#print(random.randint(0, 100))
