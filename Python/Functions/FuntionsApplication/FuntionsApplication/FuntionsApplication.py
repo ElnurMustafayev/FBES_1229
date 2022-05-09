5214ff5a1762009efb10066864f254d97ea24359
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-05-09 11:18:02 +0400

    Add Global

--- Python/Functions/FuntionsApplication/FuntionsApplication/FuntionsApplication.py
+++ Python/Functions/FuntionsApplication/FuntionsApplication/FuntionsApplication.py
@@ -1,23 +1,127 @@
-#def AskName():
-#    name = input("Enter your name: ")
-#    print(f"Hello, {name}")
+#def MyFunction():
+#    print("Start")
+#    print("...")
+#    num = 100
+#    num += 11
+#    print(f"Number is {num}")
+#    print("...")
+#    print("End")
 
-#def AskAndProceedHeight():
-#    height = int(input("Enter your height in sm: "))
-#    m = height // 100
-#    sm = height % 100
-#    print(f"You are {m} m, {sm} sm")
-#    print("\n\n\n")
+#MyFunction()
+#MyFunction()
+#MyFunction()
 
 
-#while True:
-#    AskAndProceedHeight()
-#    AskName()
 
-def Func():
-    var = 123
-    #print(var) # 123
 
-#var = 100
-Func()
-print(var) # 100
+
+
+
+#name = input("Input your name: ")
+#age = int(input("Input your age: "))
+
+#if age >= 18:
+#    print(f"{name} has access to app")
+#else:
+#    print("Access denied")
+
+
+
+
+
+
+
+#num = 100
+
+#def MyFunc():
+#    global qwerty
+#    qwerty = 100
+#    qwerty += qwerty
+#    print(qwerty)
+
+#MyFunc()
+#MyFunc()
+#print(num)
+#print(qwerty)
+
+
+
+
+
+#age = 0
+#name = ""
+
+#def InputInformation():
+#    global name
+#    global age
+
+#    name = input("Input your name: ")
+#    age = int(input("Input your age: "))
+
+
+
+#if age >= 18:
+#    print("Success")
+#else:
+#    print("Access denied")
+
+
+
+
+
+
+
+#test = 123
+
+
+#def IsUserAdult():
+#    global message
+
+#    name = input("Input your name: ")
+#    age = int(input("Input your age: "))
+
+#    message = f"{name} has Access" if age >= 18 else "Access denied!";
+
+
+
+#IsUserAdult()
+
+#print(message)
+
+
+
+
+
+
+
+
+#num = 100
+
+
+#def MyFunc():
+#    global num
+#    num = 200
+#    num *= 2
+#    print(num)
+
+#MyFunc()
+#MyFunc()
+#MyFunc()
+#print(num)
+
+
+
+
+
+
+
+# RECURSION
+#num = 0
+
+#def RecursionFunc():
+#    global num
+#    num += 1
+#    print(num)
+#    RecursionFunc()
+
+#RecursionFunc()
