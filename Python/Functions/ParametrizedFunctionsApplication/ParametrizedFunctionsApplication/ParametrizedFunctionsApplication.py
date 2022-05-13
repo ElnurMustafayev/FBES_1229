e2e1e23795a7c51532a4a387fb2e559b28038bb1
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-05-13 22:01:57 +0400

    Add Function

---
+++ Python/Functions/ParametrizedFunctionsApplication/ParametrizedFunctionsApplication/ParametrizedFunctionsApplication.py
@@ -0,0 +1,108 @@
+#num = 100
+
+#def MyFunc():
+#    global num
+#    print(num)
+
+#MyFunc()
+
+
+
+
+#print("qwe")
+#print("qwe", "123", 123, True)
+#input()
+#str(123)
+
+#type(num)
+
+
+
+
+#def MyParametrizedFunction(name):
+#    print(f"Hello, My name is {name}")
+
+
+
+## call function with variable as parameter
+#myName = "Elnur"
+#MyParametrizedFunction(myName)
+## name = myName
+
+
+## call function with value as parameter
+#MyParametrizedFunction("Bob")
+## name = "Bob"
+
+#def WhereAreYouFrom(country, city, name):
+#    print(f"Hello, my name is {name}")
+#    print(f"I am from {country}, {city}")
+
+
+#name = input("Input your name: ")
+
+#country = input("Input your birth country: ")
+#city = input("Input your birth city: ")
+
+#WhereAreYouFrom(country, city, name)
+
+
+
+
+
+#def MyMethod(num):
+#    num += 1
+#    print(num)
+
+
+
+#number = 100
+
+#MyMethod(number)
+#MyMethod(number)
+#MyMethod(number)
+
+#print(number)
+
+
+
+
+
+#def MyFunc(num):
+#    num += 10
+#    return num
+
+
+#number = 100
+#print(MyFunc(MyFunc(MyFunc(number))))
+
+
+
+
+
+#def MyFunc(name, age = 18):
+#    print(f"{name} is {age} years old")
+
+#age = 14
+#print(MyFunc("Arkhun", age))
+
+
+#def WriteError(message):
+#    print(f"Error: {message}")
+
+
+#def IsAdult(age):
+#    age = int(age)
+
+#    if age >= 18:
+#        print("Is adult")
+#    else:
+#        WriteError("Access denied!")
+
+#    return age >= 18
+
+
+
+#isAdult = IsAdult(input())
+
+#print(isAdult)
