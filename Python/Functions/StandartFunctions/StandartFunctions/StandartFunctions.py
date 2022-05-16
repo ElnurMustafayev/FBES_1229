2bb29d772a2398887eb5b2327eccf203ea43d44f
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-05-16 22:04:28 +0400

    Add standart functions

---
+++ Python/Functions/StandartFunctions/StandartFunctions/StandartFunctions.py
@@ -0,0 +1,74 @@
+#def Factorial(num):
+#    if num == 0:
+#        return 1
+#    return num * Factorial(num - 1)
+
+#result = Factorial(0)
+#print(result)
+
+
+
+
+#name = input()
+#print(name)
+
+
+
+
+#num = 12345
+
+#length = len(str(num))
+
+#print(length)
+
+
+
+#name = "john"
+#result = name.capitalize()
+#print(result)
+
+#message = "Warning: " + input("Input message: ")
+#print(message.upper())
+
+
+
+#text = "test tetst tettetstt test testetsttest"
+#wordsCount = text.count("tettetstt")
+#print(wordsCount)
+
+
+
+#word = "Error 404: Page not found!"
+#starts = word.startswith("Error  404")
+#print(starts)
+
+
+
+#searchWord = "key"
+#word = "qwertykeyjbcascbsc";
+#contains = word.__contains__(searchWord)
+
+#if contains:
+#    index = word.index(searchWord)
+#    print(f"Index of '{searchWord}' is: {str(index)}")
+#else:
+#    print(f"'{searchWord}' word not found in '{word}'")
+
+
+
+
+
+#def CheckWordsCount(text, word):
+#    text = str(text).lower()
+#    word = str(word).lower()
+
+#    if text.__contains__(word):
+#        print(text.count(word))
+#    else:
+#        print(f"{word} not found!")
+
+
+#text = input("Input text: ")
+#word = input("Input word to search: ")
+
+#CheckWordsCount(text, word)
