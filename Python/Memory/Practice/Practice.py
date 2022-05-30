9d79b278da4914bc94ff024977c5bd5c8d8b6f8b
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-05-30 11:22:10 +0400

    Add Memory

---
+++ Python/Memory/Practice/Practice.py
@@ -0,0 +1,47 @@
+#def SelectOddNumbers(array):
+#    oddNumber = []
+
+#    if array == None:
+#        return oddNumber
+
+#    for item in array:
+#        try:
+#            if item % 2 == 0:
+#                oddNumber.append(item)
+#        except Exception:
+#            continue
+
+#    return oddNumber
+
+#numbers = [13, 346, 24, "qwerty", 17, 8]
+
+#oddNumbers = SelectOddNumbers(numbers)
+
+#print(oddNumbers)
+
+
+
+
+
+
+
+#array1 = [13,346,24,8673]
+#array2 = array1.copy()
+
+#array1.remove(13)
+
+#print(array1)
+#print(array2)
+
+
+
+#names = ["Bob", "Jane", "Ann", "Mike"]
+#contains = input() in names
+#print(contains)
+
+
+
+#collection = list("qwerty")
+#collection.pop()
+#colStr = str(collection)
+#print(colStr)
