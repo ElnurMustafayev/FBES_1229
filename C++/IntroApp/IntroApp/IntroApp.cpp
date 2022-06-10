ac69c9e49e2627a8a6926e5bd1248c352448df24
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-06-10 21:42:09 +0400

    Start C++

---
+++ C++/IntroApp/IntroApp/IntroApp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int number = 100;
+	long long bigNumber = 9223372036854775807;
+
+	float floating = 123.5;
+	double floating2 = 123.5;
+
+	char symbol = 'q';
+
+	cout << number;
+
+	//cout << "Hello World!";
+}
