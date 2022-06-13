f6a2f63be74cf8b4297592743b5651fd7e3a150d
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-06-13 21:48:23 +0400

    Add data types

---
+++ C++/DataTypesApp/DataTypesApp/DataTypesApp.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+    //int number1 = 123;                                        // 4 bytes
+    //unsigned int unumber1 = 123;                              // 4 bytes
+
+    //short number2 = 32767;                                    // 2 bytes
+    //unsigned short unumber2 = 65535                           // 2 bytes
+
+    //float number3 = 155.23;                                   // 4 bytes
+    //double number4 = 155.23;                                  // 8 bytes
+     
+    //unsigned long long ubigNumber = 18446744073709551615;     // 8 bytes
+
+    //bool flag = true;                                         // 1 byte
+
+    //char symbol = '@';                                        // 1 byte
+    //unsigned char usymbol = '$';                              // 1 byte
+
+
+
+    /* 
+    
+    cout << "Hello" << " World" << '!' << "\n";
+    cout << "Hello World!" << endl << "Hello User!" << endl;
+    cout << "Hello World!\n";
+
+    cout << "\t\ate\\s\"t\n";
+
+    */
+
+    //cout << "Test"; // hello
+
+
+
+    //long long bigNum = 10147483647;
+    //int num = bigNum;
+    //cout << num << endl;
+    //cout << bigNum << endl;
+    
+    
+    
+    
+    //double num2 = 345.7;
+    //int num1 = num2;
+    
+    //cout << num1 << endl;
+    //cout << num2 << endl;
+
+
+
+    //int num = 0;
+    //bool flag = num;
+
+
+    /*int num = 4;
+    char symbol = num;
+
+    cout << num << endl;
+    cout << symbol << endl;*/
+
+
+    //int symbolNum = 254;
+    //cout << char(symbolNum) << endl;
+}
