84ff53641620a0e1c46ceb631f4b72eba9aab878
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-06-17 22:02:48 +0400

    Add operators

---
+++ C++/OperationsApplication/OperationsApplication/OperationsApplication.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //int number = 100;
+
+    /*
+    number += 10;
+    cout << number + 1 << endl;
+    cout << number << endl;
+    */
+
+    //number /= 5;
+
+    //number %= 3.5;
+
+    //cout << number / 2;
+
+    /*
+    int number = 90;
+    number += 10;
+
+    cout << number % 3 << endl;
+    */
+
+    /*int number = 5;
+
+    int result = number++ + ++number + ++number;
+
+    cout << result << endl;
+    cout << number << endl;*/
+
+
+
+    /*int number = 0;
+
+    number++;
+
+    cout << number << endl;*/
+
+
+    int number = 10;
+
+    int result = number++;
+
+    cout << result << endl;
+    cout << number << endl;
+}
