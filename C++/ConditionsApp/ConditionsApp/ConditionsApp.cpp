74170726afac32c9a78a8ab6949afb5c80518db0
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-06-20 22:00:20 +0400

    Add conditions

---
+++ C++/ConditionsApp/ConditionsApp/ConditionsApp.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /*
+    int num = 10;
+
+    cout << ++num << endl;
+    cout << num << endl;
+    */
+
+
+
+    // >, >=, <, <=, ==, !=
+
+    /*
+    bool flag = false;
+
+    flag = 10 >= 47;
+    flag = flag != false;
+
+    cout << flag << endl;
+    */
+
+
+    // and, or, not
+
+    /*
+    bool flag1 = 10 > 10;
+    bool flag2 = false;
+
+    bool result = flag1 && flag2;
+
+    result = true || false;
+
+    cout << ((result && false) || true) << endl;
+
+    cout << (false && (10 < 5)) << endl;
+    */
+
+
+    /*
+    bool flag = true;
+
+    cout << false << endl;
+    */
+
+
+
+    /*
+    cout << "start" << endl;
+
+    if (false) {
+        cout << "if" << endl;
+    }
+    else if (false) {
+        cout << "else if 1" << endl;
+    }
+    else if (false) {
+        cout << "else if 2" << endl;
+    }
+    else if (false) {
+        cout << "else if 3" << endl;
+    }
+    else {
+        cout << "else" << endl;
+    }
+
+    cout << "end" << endl;
+    */
+
+
+    //int age;
+
+    //cout << "Input your age: ";
+    //cin >> age;
+
+    //if(age > 18) {
+    //    bool isSuccess = true;
+    //}
+    //else {
+    //    bool isSuccess = false;
+    //    cout << "User is not adult" << endl;
+    //}
+
+    //cout << isSuccess << endl;
+    //
+
+
+    /*
+    int num = 123;
+
+    if (true) {
+        int num = 456;
+
+        if (true) {
+            cout << num << endl;
+        }
+        cout << num << endl;
+    }
+    cout << num << endl;
+    */
+
+
+    /*
+
+    bool check;
+    if (true) {
+        check = true;
+    }
+    else {
+        cout << check << endl;
+    }
+
+    */
+
+
+    /*
+    {
+        int age = 123;
+    }
+
+    cout << age << endl;
+    */
+
+
+
+    /*
+    if (true) 
+        int age = 123;
+
+    cout << age << endl;
+    */
+
+
+    /*
+    double money = 0;
+
+    cout << "Input your money: ";
+    cin >> money;
+
+    char hasVisa;
+    cout << "Enter your visa(y/n): ";
+    cin >> hasVisa;
+
+    cout << money << endl << (hasVisa == 'y') << endl;
+    */
+}
