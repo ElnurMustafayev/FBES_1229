bb6286cb0912315b68171854c096730c3acdeb6a
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-06-24 21:45:10 +0400

    Add while

---
+++ C++/WhileLoopApp/WhileLoopApp/WhileLoopApp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*int num = 0;
+
+	while (++num <= 5) {
+		cout << num++ << endl;
+	}
+
+	cout << num << endl;*/
+
+
+	/*
+	int num = 123;
+
+	while (--num) {
+		cout << "Test" << endl;
+	}
+	*/
+}
