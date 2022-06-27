d4926d1caf828758b75f5544619c876e6462014c
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-06-27 21:48:12 +0400

    Add For Loop

---
+++ C++/ForLoopApp/ForLoopApp/ForLoopApp.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*
+	for (int i = 0; i != 10; i += 2)
+	{
+		i += 3;
+
+		cout << i << endl;
+	}
+	*/
+
+	/*
+	int number;
+	int count;
+	cout << "Enter number: ", cin >> number;
+	cout << "Enter count to repeat: ", cin >> count;
+
+	for (int i = 0; i < count; i++)
+	{
+		cout << number << endl;
+	}
+	*/
+
+
+
+	/*
+	* from min to max
+	for (int i = 0; i <= 10; i++)
+	{
+		cout << i << endl;
+	}
+
+	* from max to min
+	for (int i = 10; i >= 0; i--)
+	{
+		cout << i << endl;
+	}
+	*/
+
+
+
+	/*
+	for (int i = 10; i != 2; i%=3)
+	{
+		cout << i << endl;
+	}
+	*/
+
+
+
+	/*
+	int num = 123;
+
+	for (;num-- - 1;)
+	{
+		cout << num << endl;
+	}
+	*/
+
+
+
+	/*
+	for (;;)
+	{
+		cout << "infinity" << endl;
+	}
+	*/
+	
+	/*
+	int i = 0;
+
+	for (cout << "hello" << endl; "start" << endl; cout << "end" << endl)
+	{
+		cout << i << endl;
+	}
+	*/
+
+
+	/*
+	for (int x = 0, y = 1; (y < 20 || x < 20); x++, y+=x++)
+	{
+		cout << x << " " << y << endl;
+	}
+	*/
+}
