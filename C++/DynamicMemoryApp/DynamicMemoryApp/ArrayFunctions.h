0f227c2089c217ce1914b1a67fefaae68a36c39f
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-07-29 21:51:23 +0400

    Add arrays functions

---
+++ C++/DynamicMemoryApp/DynamicMemoryApp/ArrayFunctions.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int* DeleteElement(int* nums, int* length) {
+	if (*length == 0 || nums == nullptr)
+		return nullptr;
+
+	int* newArray = new int[*length - 1];
+
+	// COPY OLD ELEMENTS
+	for (int i = 0; i < (*length - 1); i++)
+		newArray[i] = nums[i];
+
+	(*length)--;
+
+	return newArray;
+}
+
+int* AddElement(int* nums, int* length, int newElement) {
+	int* newArray = new int[*length + 1]{};
+
+	// COPY OLD ELEMENTS
+	for (int i = 0; i < *length; i++)
+		newArray[i] = nums[i];
+
+	// ADD NEW ELEMENT TO END
+	newArray[*length] = newElement;
+	(*length)++;
+
+	return newArray;
+
+	/*nums[*lenght] = newElement;
+	(*lenght)++;*/
+}
+
+
+
+void Print(int* arr, const int length) {
+	for (int i = 0; i < length; i++)
+		cout << arr[i] << endl;
+}
+
+
+
+
+int* CreateArray(int length) {
+	srand(time(0));
+
+	int* arr = new int[length];
+
+	for (int i = 0; i < length; i++)
+		arr[i] = rand();
+
+	return arr;
+}
+
--- C++/DynamicMemoryApp/DynamicMemoryApp/DynamicMemoryApp.cpp
+++ C++/DynamicMemoryApp/DynamicMemoryApp/DynamicMemoryApp.cpp
@@ -1,52 +1,140 @@
 #include <iostream>
+#include "ArrayFunctions.h"
 
 using namespace std;
 
-int* CreateArray(const int length);
-
 int main()
 {
-	//int nums[10];
+	/*
+		int arr1[10] = { 1,2,3 };
+		cout << arr1[8] << endl;
+		int* arr1Ptr = arr1;
+	*/
 
-	/*int length = 10;
-	int* nums = new int[length] {
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-	};*/
+	/*
+		int* arr2 = new int[];
+		cout << arr2 << endl;
+		cout << &(arr2[0]) << endl;
+	*/
 
-	//cout << nums[5] << endl;
-	//cout << *(nums + 1) << endl;
 
-	/*int num = 100;
-	cout << num << endl;*/
 
-	/*int num = 10;
-	int* numPtr = &num;
+	// int* arr = CreateArray(10);
+	// Print(arr, 10);
 
+	/*
+	int length = 10;
+	int* arr = CreateArray(length);
+	AddElement(arr, 123);
+	arr[11] = 234;
+	arr[12] = 345;
+	Print(arr, length);
+	cout << arr[10] << endl;
+	cout << arr[11] << endl;
+	cout << arr[12] << endl;
+	*/
+
+
+
+	/*int num1 = 10;
+	int num2 = 15;
+
+	int* num1Ptr = &num1;
+	int* num2Ptr = &num2;
+
+	int steps = num2Ptr - num1Ptr;
+	*(num1Ptr + steps) = 777;
+
+	cout << num2 << endl;*/
+
+
+
+	/*int length = 3;
+	int* arr = new int[length] {
+		1, 2, 3
+	};
+
+	int nums[10];
+
+	int number = 1;
+	cout << (nums - &number) << endl;
+
+	AddElement(arr, &length, 4);
+	AddElement(arr, &length, 5);
+	AddElement(arr, &length, 6);
+	AddElement(arr, &length, 7);
+	AddElement(arr, &length, 8);
+	Print(arr, length);
+
+	cout << "Number: " << number << endl;*/
+
+
+
+	/*
+	int number = -2147483648;
+	int* numberPtr = &number;
+
+	bool* byteptr = (bool*)numberPtr;
+
+	cout << *(byteptr + 0) << endl;
+	cout << *(byteptr + 1) << endl;
+	cout << *(byteptr + 2) << endl;
+	cout << *(byteptr + 3) << endl;
+	*/
+
+
+
+
+	/*
+	int length = 3;
+	int* arr = new int[length] {
+		1, 2, 3
+	};
+
+	for (int i = 0; i < 100; i++)
+		arr = AddElement(arr, &length, i);
+
+	Print(arr, length);
+	*/
+	
+
+	
+	int length = 5;
+	int* arr = new int[length] {
+		1, 2, 3, 4, 5
+	};
+	
+	/*arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	arr = DeleteElement(arr, &length);
+	cout << arr << endl;
+	Print(arr, length);*/
+	
+
+
+	//arr = DeleteElement(nullptr, &length);
+
+
+
+	/*int* numPtr = new int(10);
 	cout << numPtr << endl;
-	cout << sizeof(numPtr) << endl;*/
-
-	/*int length = 10;
-	int* newArr = CreateArray(length);
-	Print(newArr, length);*/
-
-
-	char* word = new char[100]{ "test" };
-
-	cout << word << endl;
-}
-
-int* CreateArray(const int length) {
-	//int arr[length];
-
-	int* arr = new int[length];
-
-	for (int i = 0; i < length; i++)
-		arr[i] = i;
-
-	return arr;
-}
-
-void Print(int* arr, const int length) {
-	for (int i = 0; i < length; i++)
-		cout << arr[i] << endl;
+	numPtr = 0;
+	numPtr = nullptr;
+	cout << numPtr << endl;*/
 }
