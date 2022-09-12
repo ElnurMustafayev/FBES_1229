fa06a0381116ff50067f1b696feb23721abd4491
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-09-12 21:55:38 +0400

    Update Files

--- C++/FileSystemApp/FileSystemApp/FileSystemApp.cpp
+++ C++/FileSystemApp/FileSystemApp/FileSystemApp.cpp
@@ -8,19 +8,62 @@ using namespace std;
 
 const char* filename = "test.txt";
 
-int main()
+void PrintAllTest(FILE* file, int ms);
+
+int GetLettersCountInFile(FILE* file);
+
+void WriteInFile(FILE* file, const char* word);
+
+
+
+void main()
 {
+    /*FILE* file = fopen("C:\\Users\\e.mustafayev\\Desktop\\Code\\FBES_1229\\C++\\FileSystemApp\\FileSystemApp\\test.txt", "r+");
+    FILE* file = fopen("Test\\test.txt", "r+");*/
+
     FILE* file = fopen(filename, "r+");
 
-    if (file == nullptr)
+    if (file == nullptr) {
         cout << "File '" << filename << "' not found" << endl;
-    else
-        cout << "Stream address: " << file << endl;
+        return;
+    }
 
+    //PrintAllTest(file, 50);
+    //cout << GetLettersCountInFile(file);
+
+    WriteInFile(file, "Elnur");
+}
+
+
+int GetLettersCountInFile(FILE* file) {
+    /*int counter = 0;
+    while (!feof(file)) {
+        counter++;
+
+        cout << (char)fgetc(file) << " " << counter << endl;
+    }
+
+    return counter - 1;*/
+
+    int counter = 0;
+    char symbol = ' ';
+    while ((symbol = fgetc(file)) != EOF) {
+        counter++;
+    }
+
+    return counter;
+}
+
+void PrintAllTest(FILE* file, int ms = 10) {
     int symbol = 0;
     do {
         symbol = fgetc(file);
-        Sleep(10);
+        Sleep(ms);
         cout << (char)symbol;
     } while (symbol != -1);
+}
+
+void WriteInFile(FILE* file, const char* word) {
+    for (int i = 0; word[i] != '\0'; i++)
+        fputc(word[i], file);
 }
