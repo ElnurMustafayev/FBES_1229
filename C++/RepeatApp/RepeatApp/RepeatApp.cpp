62628b9a472d9d1ed5ad37c71a557f5c0ce057eb
Elnur Mustafayev <e.mustafayev@itstep.edu.az>
2022-09-16 21:47:39 +0400

    Add const pointers

---
+++ C++/RepeatApp/RepeatApp/RepeatApp.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+
+//void SayHelloTo(const char* name) {
+//	*name = "sdasd";
+//}
+
+int main()
+{
+
+	//SayHelloTo("Arkhun");
+
+	/*int num = 100;
+	const int* numPtr = &num;
+
+	*numPtr = 123;*/
+
+	//cout << numPtr << endl;
+
+
+
+	/*const float pi = 3.14;
+
+	const float* piPtr = &pi;
+
+	*piPtr = 4;
+
+	cout << pi << endl;*/
+
+
+	// pointer to const
+	/*const char* str = "Test";
+
+	int num = 100;
+	const int* numPtr = &num;
+	num = 123;*/
+
+
+	/*int num = 100;
+	int* const numPtr = &num;
+	numPtr = &num;
+	numPtr = new int(123);
+	numPtr = numPtr;*/
+
+
+
+	/*const int num = 123;
+	const int* const superPtr = &num;*/
+}
