9611d2e1770578c1a09e0284a4197f1412f1a44d
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2022-11-28 22:09:38 +0400

    Add map & set

--- C++ OOP/BinaryTreeApp/BinaryTreeApp/BinaryTreeApp.cpp
+++ C++ OOP/BinaryTreeApp/BinaryTreeApp/BinaryTreeApp.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <map>
+#include <set>
 #include "BinaryTree.h"
 
 using namespace std;
 
+struct Person {
+	string name;
+	string surname;
+	int age;
+	string FIN;
+
+	Person(string name, string surname, int age, string FIN) : age(age), surname(surname), name(name), FIN(FIN)
+	{
+
+	}
+
+	Person() {
+
+	}
+};
+
 int main()
 {
-	BinaryTree<int> tree = BinaryTree<int>();
+	/*BinaryTree<int> tree = BinaryTree<int>();
 
 	int arr[] = { 50, 77, 34, 56, 60, 76, 25, 79, 53, 71, 94, 35, 58 };
 
@@ -15,7 +33,7 @@ int main()
 
 	tree.remove(56);
 
-	tree.show(tree.getRoot());
+	tree.show(tree.getRoot());*/
 
 	
 	/*auto root = tree.insert(10);
@@ -28,4 +46,62 @@ int main()
 	tree.remove(20);
 
 	tree.show(root);*/
+
+
+	// SET
+	/*set<string> collection = set<string>();
+
+	collection.insert("124122");
+	collection.insert("qweqex");
+	collection.insert("aaaasx");
+	collection.insert("aadzxc");
+	collection.insert("fgsdss");
+
+	for (string item : collection)
+	{
+		cout << item << endl;
+	}*/
+
+	// MAP
+
+	// FIN, PERSON
+	map<string, Person> peopleMap = map<string, Person>();
+
+	Person people[] = {
+		Person("Bob", "Bobovich", 123, "QWQWETY"),
+		Person("Ann", "Test", 42, "125YTSD"),
+		Person("Wolter", "Brown", 35, "AZAGDAS"),
+	};
+
+	for (Person person : people) {
+		// set
+		peopleMap[person.FIN] = person;
+	}
+
+	// get
+	cout << peopleMap["QWQWETY"].name << endl;
+
+	// indexers
+	/*
+	peopleMap[p1.FIN] = p1;
+	peopleMap[p2.FIN] = p2;
+	peopleMap[p3.FIN] = p3;
+	*/
+
+
+	// pairs
+	/*
+	pair<string, Person> pairToAdd1 = pair<string, Person>(p1.name, p1);
+	pair<string, Person> pairToAdd2 = pair<string, Person>(p2.name, p2);
+	pair<string, Person> pairToAdd3 = pair<string, Person>(p3.name, p3);
+
+	peopleMap.insert(pairToAdd1);
+	peopleMap.insert(pairToAdd2);
+	peopleMap.insert(pairToAdd3);
+	*/
+
+	/*for (pair<string, Person> pair : peopleMap) {
+		cout << "FIN: " << pair.first << endl;
+		cout << "Name: " << pair.second.name << endl;
+	}*/
 }
