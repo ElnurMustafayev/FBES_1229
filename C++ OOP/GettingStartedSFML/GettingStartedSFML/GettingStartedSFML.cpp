b354945e77cf31d51b96c4e8fd2b21e9e47c54ec
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2022-12-14 21:56:03 +0400

    Add Shapes

--- C++ OOP/GettingStartedSFML/GettingStartedSFML/GettingStartedSFML.cpp
+++ C++ OOP/GettingStartedSFML/GettingStartedSFML/GettingStartedSFML.cpp
@@ -1,14 +1,32 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <windows.h>
+
+using namespace std;
 
 int main()
 {
     sf::VideoMode viewMode = sf::VideoMode(500, 500);
+    sf::RenderWindow window(viewMode, "SFML Getting started!");
 
-    sf::RenderWindow window(viewMode, "It is title!");
+    sf::Shape* shape = new sf::CircleShape (100.f);
 
+    shape->setPosition(100, 100);
+
+    sf::Event event = sf::Event();
     while (window.isOpen())
     {
-        window.close();
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Magenta);
+
+        shape->setFillColor(sf::Color::Color(100, 0, 200));
+
+        window.draw(*shape);
+        window.display();
     }
 }
