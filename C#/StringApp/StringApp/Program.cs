6274808f1d83d50ceebe7c1baae7d1406846b7ce
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2023-01-06 21:54:48 +0400

    Add String

---
+++ C#/StringApp/StringApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+
+namespace StringApp {
+    class Program {
+        static void Main() {
+            /*
+            string name = "Elnur";
+            string surname = "Mustafayev";
+            string message1 = string.Format("Hello, {0} {1}", name, surname);
+            string message2 = $"Hello, {name} {surname}";
+            string message3 = @"q\we""rt/y";
+            string message4 = @"one 
+   two 
+ three";
+
+            Console.WriteLine(message1);
+            Console.WriteLine(message2);
+            Console.WriteLine(message3);
+            Console.WriteLine(message4);
+            */
+
+            //int num1 = int.Parse("123");
+            //int num2 = Convert.ToInt32("123");
+            //Console.WriteLine($"{num1} {num2}");
+
+            //int num = 123;
+            //Console.WriteLine(num.CompareTo(122));
+            //Console.WriteLine(num.CompareTo(123));
+            //Console.WriteLine(num.CompareTo(124));
+
+
+
+            //Console.OutputEncoding = Encoding.Unicode;
+            //for (int i = 0; i < 1000; i++) {
+            //    char symbol = (char)i;
+            //    Console.Write(symbol);
+            //}
+
+
+            //char ucode = '\u263C';
+            //Console.WriteLine($"{(int)ucode} {sizeof(char)} {ucode}");
+
+
+            //Console.Write("Money: ");
+            //string moneyStr = Console.ReadLine();
+            //double money = Convert.ToDouble(moneyStr);
+
+            //string personStatus = money > 10_000 ? "Rich" : "Poor";
+            //Console.WriteLine($"You are '{personStatus}'");
+
+
+            //string str = "!!!";
+
+            //string text = string.Concat("test", "\t", "salam", str);
+            //Console.WriteLine(text);
+
+            //string input = Console.ReadLine();
+            //Console.WriteLine("Null or Empty: " + string.IsNullOrEmpty(input));
+            //Console.WriteLine("Null or WhiteSpace: " + string.IsNullOrWhiteSpace(input));
+
+            //if (input != null && input.Length > 0) {
+            //if (!string.IsNullOrWhiteSpace(input)) {
+            //    int num = Convert.ToInt32(input);
+            //    Console.WriteLine(++num);
+            //}
+
+            //Console.WriteLine($"User input: \"{input}\"");
+        }
+    }
+}
