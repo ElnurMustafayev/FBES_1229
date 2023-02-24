5983501f793f08041659d972f47d8f7370676a03
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2023-02-24 21:56:08 +0400

    Add Styles

---
+++ WPF/StylesApp/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StylesApp {
+    public partial class MainWindow : Window, INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Brush counterColor = Brushes.Black;
+
+        public Brush CounterColor { 
+            get => counterColor;
+            set => ChangeProperty(out this.counterColor, value);
+        }
+
+        public void ChangeProperty<T>(out T field, T value, [CallerMemberName] string propName = "") {
+            field = value;
+            this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+
+        public ObservableCollection<User> Users {
+            get { return users; }
+            set { users = value; }
+        }
+
+
+
+
+        public MainWindow() {
+            InitializeComponent();
+
+            this.DataContext = this;
+
+            this.Users.Add(new User("David", "Kashirskiy", 15));
+            this.Users.Add(new User("Diana", "Ahadova", 16));
+            this.Users.Add(new User("Samra", "Mamedli", 17));
+        }
+
+
+
+        private void MyTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            //var isOnLimit = this.MyTextBox.Text.Length >= 10;
+            //
+            //this.CounterColor = isOnLimit ? Brushes.Red : Brushes.Black;
+            //
+            //if(isOnLimit) {
+            //    this.MyTextBox.IsEnabled = false;
+            //}
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            var newUser = new User(
+                name: Guid.NewGuid().ToString("N")[..6],
+                surname: Guid.NewGuid().ToString("N")[..6],
+                age: new Random().Next(60));
+
+            this.Users.Add(newUser);
+
+            //MessageBox.Show($"User added. Count: {this.Users.Count}");
+        }
+    }
+}
---
+++ WPF/StylesApp/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StylesApp {
+    public class User {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, string surname, int age) {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public override string ToString() => $"{Name} {Surname}: {Age} years old";
+    }
+}
