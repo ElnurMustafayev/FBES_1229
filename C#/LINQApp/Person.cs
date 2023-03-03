76865fe281766fd23dd7fe5c5ee4d2469401b4ef
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2023-03-03 21:56:27 +0400

    End LINQ

---
+++ C#/LINQApp/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace LINQApp;
+
+public enum Gender {
+    Male, Female, Other, Unknown
+}
+
+public class Person : IComparable<Person> {
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public bool IsMarried { get; set; } = false;
+    public Gender Gender { get; set; }
+    public IEnumerable<int> Marks { get; set; }
+
+    public int CompareTo(Person? other) {
+        return (this.Age - other.Age);
+    }
+}
--- C#/LINQApp/Program.cs
+++ C#/LINQApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
 
 namespace LINQApp;
 
@@ -29,9 +30,209 @@ class Program {
 
     delegate void MyParametrizedDel<T>(T arg, int num);
 
+    static decimal ReturnDefaultValue2() => default;
+    static decimal ReturnDefaultValue1() {
+        return default;
+    }
     static void Main() {
+
+        // Any
+        // All
+        // Count
+        // Min
+        // Max
+        // First
+        // FirstOrDefault
+        // Last
+        // LastOrDefault
+        // Sum
+        // Average
+        // Skip
+        // Take
+        // Where
+        // Select
+
+        IEnumerable<Person> people = new List<Person>() {
+            new Person() {
+                Name = "Kate",
+                Age = 16,
+                Gender = Gender.Female,
+                IsMarried = false,
+                Marks = new int[] {3,6,3,10},
+            },
+
+            new Person() {
+                Name = "Zabil",
+                Age = 29,
+                Gender = Gender.Male,
+                IsMarried = true,
+                Marks = new int[] {12,11,10,12},
+            },
+
+            new Person() {
+                Name = "Bob",
+                Age = 45,
+                IsMarried = false,
+                Gender = Gender.Unknown,
+                Marks = new int[] {10, 9, 7, 5},
+            },
+
+            new Person() {
+                Name = "Samra",
+                Age = 18,
+                IsMarried = false,
+                Gender = Gender.Female,
+                Marks = new int[] {12, 10, 12, 9},
+            },
+
+            new Person() {
+                Name = "Arkhun",
+                Age = 15,
+                IsMarried = false,
+                Gender = Gender.Male,
+                Marks = new int[] {9, 9, 12, 10},
+            },
+
+            new Person() {
+                Name = "Ann",
+                Age = 17,
+                IsMarried = true,
+                Gender = Gender.Female,
+                Marks = new int[] {12, 12, 12, 12},
+            },
+        };
+
+        //bool containsAdult = people.Any(person => person.Age >= 18);
+        //bool allAreAdult = people.All(p => p.Age >= 18);
+        //Console.WriteLine(allAreAdult);
+
+        //Console.WriteLine($"Count: {people.Count()}");
+
+        //int count = people.Count(p => p.Age >= 18 && p.IsMarried);
+
+        //count = people.Count(p => {
+        //    if(string.IsNullOrWhiteSpace(p.Name)) {
+        //        return false;
+        //    }
+        //    else if (p.Name.Length < 5) {
+        //        return false;
+        //    }
+        //    else {
+        //        return p.Name[4] == 'a';
+        //    }
+        //});
+
+        //count = people.Count(p => p.Gender == Gender.Unknown || p.Gender == Gender.Other);
+
+        //Console.WriteLine(count);
+
+        //var minAge = people.Min(p => p.Age);
+        //var maxAge = people.Max(p => p.Age);
+        //
+        //Console.WriteLine(minAge);
+        //Console.WriteLine(maxAge);
+
+        //var personMin = people.Min();
+        //var personMax = people.Max();
+
+        //Console.WriteLine(personMin.Name);
+        //Console.WriteLine(personMax.Name);
+
+
+        //var found = people.FirstOrDefault(p => p.Age == 19);
+        //found = people.LastOrDefault(p => p.Gender == Gender.Female);
+        //if (found == null) {
+        //    Console.WriteLine("Person not found!");
+        //}
+        //else {
+        //    Console.WriteLine($"Found person's name: {found.Name}");
+        //}
+
+        //var allAges = people.Sum(p => p.Age);
+        //var avgAge = (int)people.Average(p => p.Age);
+        //Console.WriteLine(allAges);
+        //Console.WriteLine(avgAge);
+
+        //var result = people.Skip(2).Take(2).Skip(0);
+        //
+        //foreach (var person in result) {
+        //    Console.WriteLine(person.Name);
+        //}
+
+        //var adults = people.Where(p => p.Age >= 18);
+        //var founds = people.Where(p => p.Age < 18 && p.IsMarried);
+
+        //foreach (var person in founds) {
+        //    Console.WriteLine($"{person.Name}: {person.Age} years old");
+        //}
+
+
+        // get avg age of marrieds
+        //var avgAgeOfMarrieds = people
+        //    .Where(p => p.IsMarried)
+        //    .Average(p => p.Age);
+
+        //Console.WriteLine(avgAgeOfMarrieds);
+
+
+        //var avgMark = people
+        //    .Select(p => p.Marks.Average())
+        //    .Average();
+
+        //Console.WriteLine($"Avg mark is {avgMark}");
+
+        //var goodStudents = people
+        //    .Where(p => p.Marks.Average() >= avgMark);
+
+        //foreach (var student in goodStudents) {
+        //    Console.WriteLine(student.Name);
+        //}
+
+
+
+
+
+        //IEnumerable<int> collection = new List<int>() {
+        //    12,5,15,45,7,43,23,575,714,5
+        //};
+
+        //Console.WriteLine($"Min: {collection.Min()}");
+        //Console.WriteLine($"Max: {collection.Max()}");
+
+        //var result = collection.Any((num) => new Random().Next(0, 10) == 5);
+
+        //var result = collection.All(num => num % 5 == 0);
+
+        //Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // anonimous function
-        if(false) {
+        if (false) {
 
             Action action = new Action(delegate () {
                 Console.WriteLine("Big");
