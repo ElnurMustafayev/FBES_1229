f00e95de25d2b78f96dfa98a1b673cfe57577e1f
Mustafayev Elnur Ilham <elnur.mustafayeev@gmail.com>
2023-04-17 21:46:57 +0400

    Add Sql

---
+++ Sql/Create.sql
@@ -0,0 +1,12 @@
+--create table TestTable(
+--One char,
+--Two int,
+--Three NVarChar(10),
+--)
+
+
+--create table Products (
+--	[Name] nvarchar(20),
+--	Price money,
+--	[Weight] decimal
+--)
---
+++ Sql/Select.sql
@@ -0,0 +1,15 @@
+--select * from Products
+
+--select u.[Name] as 'Ad', u.Surname as 'Soyad'
+--from Users u
+
+--select u.Name, Surname, u.Age
+--from Users u
+
+--select Surname, Name
+--from Users
+
+--select Users.Surname, Users.Name, Age
+--from Users
+
+-- var result = users.Select(u => u.Name);
