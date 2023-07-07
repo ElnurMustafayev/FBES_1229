a0f9d99cc2e1f5b08adb37fb55804c652a14e02c
Mustafayev Elnur Ilham <elnur.mustafayeev@gmail.com>
2023-07-07 21:59:54 +0400

    Add Lock

---
+++ System Programming/LockApp/LockApp/Program.cs
@@ -0,0 +1,68 @@
+﻿public class Program {
+    private static void Main() {
+        if(false) {
+            Mutex mutex = new Mutex(true, "MyMutex", out bool createdNew);
+
+            if (createdNew == false)
+                throw new Exception("Application already opened!");
+
+            Console.ReadKey();
+        }
+
+
+        if(false) {
+            Mutex mutex = new Mutex(false, "MyMutex");
+            bool result = mutex.WaitOne();
+            Console.WriteLine(result);
+            Console.ReadKey();
+            mutex.ReleaseMutex();
+            Console.ReadKey();
+        }
+
+
+        if(true) {
+            object locker = new object();
+            int num = 0;
+
+            //Mutex mutex = new Mutex();
+            //Semaphore semaphore = new Semaphore(1, 1);
+
+            var task1 = Task.Run(() => {
+                Console.WriteLine("Task 1 start.");
+                for (int i = 0; i < 1_500_000; i++) {
+                    //Interlocked.Increment(ref num);
+                    //mutex.WaitOne();
+                    //semaphore.WaitOne();
+                    //Monitor.Enter(locker);
+                    lock (locker) {
+                        num++;
+                    }
+                    //Monitor.Exit(locker);
+                    //semaphore.Release();
+                    //mutex.ReleaseMutex();
+                }
+                Console.WriteLine("Task 1 end.");
+            });
+
+            var task2 = Task.Run(() => {
+                Console.WriteLine("Task 2 start.");
+                for (int i = 0; i < 2_000_000; i++) {
+                    //Interlocked.Increment(ref num);
+                    //mutex.WaitOne();
+                    //semaphore.WaitOne();
+                    //Monitor.Enter(locker);
+                    lock (locker) {
+                        num++;
+                    }
+                    //Monitor.Exit(locker);
+                    //semaphore.Release();
+                    //mutex.ReleaseMutex();
+                }
+                Console.WriteLine("Task 2 end.");
+            });
+
+            Task.WaitAll(task1, task2);
+            Console.WriteLine(num);
+        }
+    }
+}
--- System Programming/TasksApp/TasksApp/Program.cs
+++ System Programming/TasksApp/TasksApp/Program.cs
@@ -16,6 +16,63 @@ public class Program {
 
     static void Main() {
         if(false) {
+            Task<string> task = Task.Run<string>(() => {
+                Thread.Sleep(3000);
+                return Guid.NewGuid().ToString();
+            });
+
+            Console.WriteLine(task.Result);
+        }
+
+
+        if(false) {
+            Task<int> task = new Task<int>(() => {
+                Thread.Sleep(10000);
+                return 10;
+            });
+            task.Start();
+
+            var num = task.Result;
+
+            Console.WriteLine(num);
+        }
+
+
+        if(false) {
+            var task = Task.Run(() => {
+                Console.WriteLine("Hello World!");
+            });
+
+            Task.WaitAll(task);
+        }
+
+        if(false) {
+            var task1 = new Task(() => {
+                var threadId = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine($"Thread {threadId} start...");
+                Thread.Sleep(5000);
+                Console.WriteLine($"Thread {threadId} end...");
+            });
+            task1.Start();
+
+            var task2 = new Task(() => {
+                var threadId = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine($"Thread {threadId} start...");
+                Thread.Sleep(1500);
+                Console.WriteLine($"Thread {threadId} end...");
+            });
+            task2.Start();
+
+            //Task.WaitAny(task1, task2);
+            Task.WaitAll(task1, task2);
+            Console.WriteLine("All tasks done!");
+        }
+
+
+
+
+
+        if(false) {
             var task = AsyncMethod();
             task.Wait();
 
