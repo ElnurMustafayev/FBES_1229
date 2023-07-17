fecaf6aab3fd09b08a26bd8bfb39557b8a52d8ba
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2023-07-17 16:44:41 +0400

    Add Network Programming

---
+++ Network Programming/Socket/ClientServerApp/ClientApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ClientApp;
+
+public class Program {
+    public static async Task Main() {
+        Socket socket = new Socket(
+            AddressFamily.InterNetwork,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+        const string serverAddress = "127.0.0.1";
+        const short port = 8080;
+        await socket.ConnectAsync(serverAddress, port);
+        Console.WriteLine($"Client connected to {serverAddress}:{port}!");
+
+        bool serverIsWorking = true;
+
+        ThreadPool.QueueUserWorkItem(async (state) => {
+            byte[] buffer = new byte[1024];
+            while(serverIsWorking)
+            {
+                try
+                {
+                    int messageSize = await socket.ReceiveAsync(buffer, SocketFlags.None);
+                    string messageFromClient = Encoding.UTF8.GetString(buffer, 0, messageSize);
+                    Console.WriteLine($"Answer from server: '{messageFromClient}'");
+                }
+                catch(SocketException)
+                {
+                    Console.WriteLine("Server turned off");
+                    serverIsWorking = false;
+                }
+            }
+        });
+
+        Console.WriteLine("Send Message to server: ");
+        while(serverIsWorking)
+        {
+            string? message = Console.ReadLine();
+            if (serverIsWorking && string.IsNullOrEmpty(message) == false)
+            {
+                byte[] messageInBytes = Encoding.UTF8.GetBytes(message);
+                await socket.SendAsync(messageInBytes, SocketFlags.None);
+            }
+        }
+    }
+}
---
+++ Network Programming/Socket/ClientServerApp/ServerApp/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace ServerApp;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Program {
+    public static async Task Main() {
+        Socket serverSocket = new Socket(
+            AddressFamily.InterNetwork, 
+            SocketType.Stream, 
+            ProtocolType.Tcp);
+
+        //var ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
+        const string serverAddress = "127.0.0.1";
+        const short port = 8080;
+        var ipAddress = IPAddress.Parse(serverAddress);
+        var endpoint = new IPEndPoint(ipAddress, port);
+
+        serverSocket.Bind(endpoint);
+        serverSocket.Listen(5);
+        Console.WriteLine($"Server started on {serverAddress}:{port}!");
+
+        while(true)
+        {
+            Socket clientSocket = await serverSocket.AcceptAsync();
+            Console.WriteLine("Client connected!");
+
+            // For client
+            ThreadPool.QueueUserWorkItem(async (state) =>
+            {
+                try
+                {
+                    Console.WriteLine("Client Start");
+
+                    while(true)
+                    {
+                        byte[] buffer = new byte[1024];
+                        int messageSize = await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
+                        Console.WriteLine("Message received!");
+
+                        string messageFromClient = Encoding.UTF8.GetString(buffer, 0, messageSize);
+                        Console.WriteLine($"Message: '{messageFromClient}'");
+
+                        if(messageFromClient.ToLower().Contains("bob")) {
+                            var messageToClient = Encoding.UTF8.GetBytes("Hello, Bob!");
+                            await clientSocket.SendAsync(messageToClient, SocketFlags.None);
+                        }
+                    }
+                }
+                catch(SocketException)
+                {
+                    Console.WriteLine("Client disconnected!");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"System error: '{ex.Message}'");
+                }
+                finally
+                {
+                    Console.WriteLine("Client End");
+                }
+            });
+        }
+    }
+}
