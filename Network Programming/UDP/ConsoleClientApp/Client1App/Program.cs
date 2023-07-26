63e7b34d0c16fc9d102cbff240e07bc63c55af18
Mustafayev Elnur Ilham <elnur.mustafayeev@gmail.com>
2023-07-26 21:41:24 +0400

    Add UDP

--- Network Programming/UDP/ConsoleClientApp/Client1App/Program.cs
+++ Network Programming/UDP/ConsoleClientApp/Client1App/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var udpClient = new UdpClient(8080);
+const int updPacketSizeLimit = 65000;
+
+#region Allow UDP receive after send
+
+    const uint IOC_IN = 0x80000000U;
+    const uint IOC_VENDOR = 0x18000000U;
+
+    // Controls whether UDP PORT_UNREACHABLE messages are reported. 
+    const int SIO_UDP_CONNRESET = unchecked((int)(IOC_IN | IOC_VENDOR | 12));
+    udpClient.Client.IOControl(SIO_UDP_CONNRESET, new byte[] { 0x00 }, null);
+
+#endregion
+
+ThreadPool.QueueUserWorkItem(async (state) => {
+    while(true) {
+        var updReceiveResult = await udpClient.ReceiveAsync();
+
+        var message = Encoding.UTF8.GetString(updReceiveResult.Buffer);
+
+        Console.WriteLine($"{updReceiveResult.RemoteEndPoint}: {message}");
+    }
+});
+
+//send
+while (true) {
+    Console.Write("Put Image path: ");
+    var imagePath = Console.ReadLine();
+
+    if (File.Exists(imagePath)) {
+        var imageInBytes = File.ReadAllBytes(imagePath);
+
+        for (int packetNumber = 0; true; packetNumber++) {
+            byte[] packetToSend = imageInBytes
+                .Skip(packetNumber * updPacketSizeLimit)
+                .Take(updPacketSizeLimit)
+                .ToArray();
+
+            if(packetToSend.Length == 0) {
+                byte[] endMessage = Encoding.UTF8.GetBytes("image end");
+
+                await udpClient.SendAsync(
+                    datagram: endMessage,
+                    bytes: endMessage.Length,
+                    endPoint: new IPEndPoint(
+                        address: IPAddress.Parse("127.0.0.1"),
+                        port: 8000)
+                    );
+
+                break;
+            }
+
+            int size = await udpClient.SendAsync(
+                datagram: packetToSend,
+                bytes: packetToSend.Length,
+                endPoint: new IPEndPoint(
+                    address: IPAddress.Parse("127.0.0.1"),
+                    port: 8000)
+                );
+                
+            Console.WriteLine($"{packetNumber} packet for '{new FileInfo(imagePath).Name}' image");
+        }
+    }
+}
--- Network Programming/UDP/ConsoleClientApp/Client2App/Program.cs
+++ Network Programming/UDP/ConsoleClientApp/Client2App/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+
+var udpClient = new UdpClient(8000);
+
+const string stopMessage = "image end";
+
+int stopMessageSize = Encoding.UTF8.GetBytes(stopMessage).Length;
+
+// receive
+while (true) {
+    var newFileName = $"{Guid.NewGuid()}.png";
+    Console.WriteLine($"File {newFileName} started...");
+
+    using FileStream fileStream = new FileStream(
+        path: $"data\\{newFileName}",
+        mode: FileMode.OpenOrCreate);
+
+    int packetsCount = 0;
+    for (packetsCount = 0; true; packetsCount++) {
+        var result = await udpClient.ReceiveAsync();
+        Console.WriteLine($"Packet (Size: {result.Buffer.Length} bytes) received for image: {newFileName}!");
+
+        if (result.Buffer.Length == stopMessageSize && Encoding.UTF8.GetString(result.Buffer, 0, stopMessageSize) == stopMessage)
+            break;
+
+        await fileStream.WriteAsync(result.Buffer, 0, result.Buffer.Length);
+    }
+
+    await fileStream.FlushAsync();
+    Console.WriteLine($"'{newFileName}' packets count: {packetsCount}");
+}
+
+Console.ReadKey();
