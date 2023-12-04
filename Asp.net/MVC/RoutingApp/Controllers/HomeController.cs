81f45cae3e81ca6c4ce73d347391ad6e57c6830c
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2023-12-04 21:46:29 +0400

    Add Asp.net routing

---
+++ Asp.net/MVC/RoutingApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿namespace RoutingApp.Controllers;
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RoutingApp.Models;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return base.View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return base.View();
+    }
+
+    public IActionResult Test() {
+        return base.View("RandomName");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Asp.net/MVC/RoutingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RoutingApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
