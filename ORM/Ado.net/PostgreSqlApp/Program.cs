702cf8faa82f0ce40170ef69d3f0559673e3e965
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-03-06 21:58:37 +0400

    Add PostgresSql

---
+++ ORM/Ado.net/PostgreSqlApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql;
+
+var host = "akshambazari.postgres.database.azure.com";
+var login = "akhshambazari";
+var password = "Bazar12345!";
+var database = "postgres";
+var port = 5432;
+
+var connectionString = $"User ID={login};Password={password};Host={host};Port={port};Database={database};";
+
+using var connection = new NpgsqlConnection(connectionString);
+await connection.OpenAsync();
+
+
+for (int i = 0; i < 100; i++)
+{
+    using var command = new NpgsqlCommand($"create table TEST{Guid.NewGuid().ToString("N").ToUpper()} ( myColumn varchar(100) )", connection);
+
+    var result = await command.ExecuteScalarAsync();
+}
