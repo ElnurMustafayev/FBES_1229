cd0043cce89ba16e8f6b6dd3dcb0df712d36c481
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-05-03 21:53:10 +0400

    Add Dockerfile for WebApi

--- Docker/dotnet/ConsoleApp/Program.cs
+++ Docker/dotnet/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿while (true) {
+﻿/*
+while (true) {
     Thread.Sleep(1000);
 
     var processEnvironmentVariables = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
@@ -9,4 +10,33 @@
     }
 
     System.Console.WriteLine("* * * \n\n\n");
+}
+*/
+
+
+
+// Создать универсальный образ, при создании контейнера которого 
+// будет предоставлен следующий функционал: 
+//   
+//   заменa слов TICK и TACK (обязательные параметры)
+//   задачa интервала таймера (по умолчанию 1 секунда)
+
+var tickWord = Environment.GetEnvironmentVariable("TICK");
+ArgumentNullException.ThrowIfNullOrEmpty(tickWord);
+
+var tackWord = Environment.GetEnvironmentVariable("TACK");
+ArgumentNullException.ThrowIfNullOrEmpty(tackWord);
+
+int timerIntervalMs = 1000;
+
+var timerIntervalStr = Environment.GetEnvironmentVariable("INTERVAL");
+if(int.TryParse(timerIntervalStr, out int timerIntervalMsResult)) {
+    timerIntervalMs = timerIntervalMsResult;
+}
+
+int counter = 0;
+while (true)
+{
+    System.Console.WriteLine((counter++ % 2 == 1) ? tackWord : tickWord);
+    Thread.Sleep(timerIntervalMs);
 }
